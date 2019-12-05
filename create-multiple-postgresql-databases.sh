276cd08c7979767a5d490752a94acc5510ac3636
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-05 12:31:16 +0530

    Update create-multiple-postgresql-databases.sh

--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -6,9 +6,8 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON  $database TO $POSTGRES_USER;
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
+	    CREATE DATABASE $database OWNER $POSTGRES_USER;
 EOSQL
 }
 
