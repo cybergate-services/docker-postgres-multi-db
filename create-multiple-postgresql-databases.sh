ae9f701431206aa7f8c82ceb2603dc5eac3180ff
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-05 10:11:29 +0530

    Update create-multiple-postgresql-databases.sh

--- create-multiple-postgresql-databases.sh
+++ create-multiple-postgresql-databases.sh
@@ -9,7 +9,7 @@ function create_user_and_database() {
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOSQL
 	    CREATE ROLE $owner WITH LOGIN PASSWORD '$POSTGRES_PASSWORD';
-	    CREATE DATABASE $database OWNER $owner;
+	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON  $database TO $owner;
 EOSQL
 }
